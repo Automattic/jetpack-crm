7aea499ead3e2bfdaaca9e86b78f8386a13f75e8
sdixon194 <sdixon194@users.noreply.github.com>
2023-01-13 15:50:16 +0000

    Add CRM to the Monorepo (#28314)
    Co-authored-by: gogdzl <diego.rodrigues@automattic.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    Co-authored-by: sdixon194 <steve.dixon@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3912690017

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+# Changelog
+
+### This is a list detailing changes for all Jetpack CRM releases.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [5.5.2-alpha] - unreleased
+
+This is an alpha version! The changes listed here are not final.
+
+## 5.5.1 - 2022-12-16
+
+- Fixed: Inline field editing no longer prevents listings from being displayed
+- Improved: Security around phone numbers viewing
+- Improved: Added a migration to remove outdated AKA lines
+
+[5.5.2-alpha]: https://github.com/Automattic/zero-bs-crm/compare/v5.5.1...v5.5.2-alpha
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'automattic/jetpackcrm',
+        'pretty_version' => 'dev-trunk',
+        'version' => 'dev-trunk',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'automattic/jetpackcrm' => array(
+            'pretty_version' => 'dev-trunk',
+            'version' => 'dev-trunk',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'automattic/woocommerce' => array(
+            'pretty_version' => '3.1.0',
+            'version' => '3.1.0.0',
+            'reference' => 'd3b292f04c0b3b21dced691ebad8be073a83b4ad',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../automattic/woocommerce',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
