fd4fffd0c19ab8b42389cc96231ffb471dca071d
kozer <kozer@users.noreply.github.com>
2023-03-24 12:53:04 +0000

    Subscriptions: Allow user to enable Subscriptions from Subscribe Block and pre-publish panel (#29044)
    * Subscriptions: Add pre publish ui when feature is disabled
    
    * Update: remove lodash get
    
    * [not verified] Subscriptions: Add hook to toggle module and finish UI
    
    * Subscriptions: Add more explaination on why its good to enable subscriptions
    
    * Fix: subscription module tests
    
    * Tests: Add subscription placeholder tests
    
    * useModuleStatus: Add different loading for modules and status changing to avoid flickerings on slow connections
    
    * Add: Add changelog files
    
    * Update: Update shared-extension-utils version
    
    * Update: minor wordking changes based on pr comments
    
    * Update: Remove activate subscription component from post-publish panel
    
    * Update: Update hook changelog
    
    * Tests: Add tests for use-module-status hook
    
    * Update: Update shared-extension-utils package json
    
    * Add: Add subscription skeleton loader component
    
    * Update: pnpm lock file
    
    * Add dummy argument to avoid production build failures
    
    For some reason the build doesn't handle translations in ternaries very well.
    
    * changelog
    
    * Update: pnpm.lock file
    
    * Fix: fix test_comments_are_displaying_on_not_accessible_pages test
    
    * Make it easier to run tests
    
    * Update: remove filter from jetpack-subscriptions test
    
    * Update: fix lint errors
    
    * Fix: lint errors on use-module-status
    
    * Fix: lint errors on subscriptions panel
    
    ---------
    
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4511152406

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-assets' => array(
             'pretty_version' => '1.17.34',
             'version' => '1.17.34.0',
-            'reference' => '7ccd3174857e4b478f04646622b89abfbdd2facb',
+            'reference' => 'd2ab377f94cc7c933efa3bd4bb12fa9cd89a4910',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-autoloader' => array(
             'pretty_version' => '2.11.16',
             'version' => '2.11.16.0',
-            'reference' => '64404c37bc2b862f92b63216a58e8a3153406faa',
+            'reference' => 'ce4eaabd6331e7539d97655478d535bdb37caab9',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-composer-plugin' => array(
             'pretty_version' => '1.1.10',
             'version' => '1.1.10.0',
-            'reference' => '1d275bc3372e583bb1c51cb85f1e4b869a82e2bb',
+            'reference' => '576588c401dcc732f324bfe2adda6794719b4a6e',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-composer-plugin',
             'aliases' => array(),
@@ -40,7 +40,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '1.6.21',
             'version' => '1.6.21.0',
-            'reference' => '421de3b89ce098c9b297df7f2a032f53ec48add5',
+            'reference' => 'f51336d9682e06323a3e2d2e0818b100dc8f3a6f',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-constants',
             'aliases' => array(),
