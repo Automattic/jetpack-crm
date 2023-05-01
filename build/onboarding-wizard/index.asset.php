4e8dff58d36322997d08e1e87173e0c0b586f6c8
anomiex <anomiex@users.noreply.github.com>
2023-05-01 14:06:06 +0000

    Update dependency classnames to v2.3.2 (#30378)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4851658890

--- build/onboarding-wizard/index.asset.php
+++ build/onboarding-wizard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '425f07cb35b9651091dd');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '5393b9e7a816208bd422');
