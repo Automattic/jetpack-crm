344876811cd7fc771bf836a7b5bfcc5870ca0060
coder-karen <coder-karen@users.noreply.github.com>
2023-05-24 16:31:08 +0000

    CRM: Removing jetpack-forms as a required dependency in composer.json (#30894)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5070949895

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -18,14 +18,6 @@ return array(
 		'version' => '2.11.21.0',
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/class-autoloader-locator.php'
 	),
-	'Automattic\\Jetpack\\A8c_Mc_Stats' => array(
-		'version' => '1.4.20.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-a8c-mc-stats/src/class-a8c-mc-stats.php'
-	),
-	'Automattic\\Jetpack\\Admin_UI\\Admin_Menu' => array(
-		'version' => '0.2.20.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-admin-ui/src/class-admin-menu.php'
-	),
 	'Automattic\\Jetpack\\Assets' => array(
 		'version' => '1.18.3.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/src/class-assets.php'
@@ -54,10 +46,6 @@ return array(
 		'version' => '2.11.21.0',
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/ManifestGenerator.php'
 	),
-	'Automattic\\Jetpack\\Blocks' => array(
-		'version' => '1.4.22.0-alpha',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-blocks/src/class-blocks.php'
-	),
 	'Automattic\\Jetpack\\Composer\\Manager' => array(
 		'version' => '1.1.11.0',
 		'path'    => $vendorDir . '/automattic/jetpack-composer-plugin/src/class-manager.php'
@@ -66,213 +54,9 @@ return array(
 		'version' => '1.1.11.0',
 		'path'    => $vendorDir . '/automattic/jetpack-composer-plugin/src/class-plugin.php'
 	),
-	'Automattic\\Jetpack\\Connection\\Client' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-client.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Connection_Notice' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-connection-notice.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Error_Handler' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-error-handler.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Initial_State' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-initial-state.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Manager' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-manager.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Manager_Interface' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/interface-manager.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Nonce_Handler' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-nonce-handler.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Package_Version' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-package-version.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Package_Version_Tracker' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-package-version-tracker.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Plugin' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-plugin.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Plugin_Storage' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-plugin-storage.php'
-	),
-	'Automattic\\Jetpack\\Connection\\REST_Connector' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-rest-connector.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Rest_Authentication' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-rest-authentication.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Secrets' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-secrets.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Server_Sandbox' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-server-sandbox.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Tokens' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-tokens.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Urls' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-urls.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Utils' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-utils.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Webhooks' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-webhooks.php'
-	),
-	'Automattic\\Jetpack\\Connection\\Webhooks\\Authorize_Redirect' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/webhooks/class-authorize-redirect.php'
-	),
-	'Automattic\\Jetpack\\Connection\\XMLRPC_Async_Call' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-xmlrpc-async-call.php'
-	),
-	'Automattic\\Jetpack\\Connection\\XMLRPC_Connector' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-xmlrpc-connector.php'
-	),
 	'Automattic\\Jetpack\\Constants' => array(
 		'version' => '1.6.22.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-constants/src/class-constants.php'
-	),
-	'Automattic\\Jetpack\\CookieState' => array(
-		'version' => '1.17.1.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-cookiestate.php'
-	),
-	'Automattic\\Jetpack\\Errors' => array(
-		'version' => '1.17.1.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-errors.php'
-	),
-	'Automattic\\Jetpack\\Files' => array(
-		'version' => '1.17.1.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-files.php'
-	),
-	'Automattic\\Jetpack\\Forms\\ContactForm\\Admin' => array(
-		'version' => '0.19.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-forms/src/contact-form/class-admin.php'
-	),
-	'Automattic\\Jetpack\\Forms\\ContactForm\\Contact_Form' => array(
-		'version' => '0.19.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-forms/src/contact-form/class-contact-form.php'
-	),
-	'Automattic\\Jetpack\\Forms\\ContactForm\\Contact_Form_Endpoint' => array(
-		'version' => '0.19.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-forms/src/contact-form/class-contact-form-endpoint.php'
-	),
-	'Automattic\\Jetpack\\Forms\\ContactForm\\Contact_Form_Field' => array(
-		'version' => '0.19.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-forms/src/contact-form/class-contact-form-field.php'
-	),
-	'Automattic\\Jetpack\\Forms\\ContactForm\\Contact_Form_Plugin' => array(
-		'version' => '0.19.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-forms/src/contact-form/class-contact-form-plugin.php'
-	),
-	'Automattic\\Jetpack\\Forms\\ContactForm\\Contact_Form_Shortcode' => array(
-		'version' => '0.19.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-forms/src/contact-form/class-contact-form-shortcode.php'
-	),
-	'Automattic\\Jetpack\\Forms\\ContactForm\\Editor_View' => array(
-		'version' => '0.19.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-forms/src/contact-form/class-editor-view.php'
-	),
-	'Automattic\\Jetpack\\Forms\\ContactForm\\Form_View' => array(
-		'version' => '0.19.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-forms/src/contact-form/class-form-view.php'
-	),
-	'Automattic\\Jetpack\\Forms\\ContactForm\\Util' => array(
-		'version' => '0.19.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-forms/src/contact-form/class-util.php'
-	),
-	'Automattic\\Jetpack\\Forms\\Dashboard\\Dashboard' => array(
-		'version' => '0.19.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-forms/src/dashboard/class-dashboard.php'
-	),
-	'Automattic\\Jetpack\\Forms\\Dashboard\\Dashboard_View_Switch' => array(
-		'version' => '0.19.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-forms/src/dashboard/class-dashboard-view-switch.php'
-	),
-	'Automattic\\Jetpack\\Forms\\Jetpack_Forms' => array(
-		'version' => '0.19.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-forms/src/class-jetpack-forms.php'
-	),
-	'Automattic\\Jetpack\\Forms\\Service\\Google_Drive' => array(
-		'version' => '0.19.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-forms/src/service/class-google-drive.php'
-	),
-	'Automattic\\Jetpack\\Forms\\Service\\Post_To_Url' => array(
-		'version' => '0.19.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-forms/src/service/class-post-to-url.php'
-	),
-	'Automattic\\Jetpack\\Forms\\WPCOM_REST_API_V2_Endpoint_Forms' => array(
-		'version' => '0.19.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-forms/src/class-wpcom-rest-api-v2-endpoint-forms.php'
-	),
-	'Automattic\\Jetpack\\Heartbeat' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-heartbeat.php'
-	),
-	'Automattic\\Jetpack\\Modules' => array(
-		'version' => '1.17.1.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-modules.php'
-	),
-	'Automattic\\Jetpack\\Paths' => array(
-		'version' => '1.17.1.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-paths.php'
-	),
-	'Automattic\\Jetpack\\Redirect' => array(
-		'version' => '1.7.25.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-redirect/src/class-redirect.php'
-	),
-	'Automattic\\Jetpack\\Roles' => array(
-		'version' => '1.4.23.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-roles/src/class-roles.php'
-	),
-	'Automattic\\Jetpack\\Status' => array(
-		'version' => '1.17.1.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-status.php'
-	),
-	'Automattic\\Jetpack\\Status\\Cache' => array(
-		'version' => '1.17.1.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-cache.php'
-	),
-	'Automattic\\Jetpack\\Status\\Host' => array(
-		'version' => '1.17.1.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-host.php'
-	),
-	'Automattic\\Jetpack\\Status\\Visitor' => array(
-		'version' => '1.17.1.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-status/src/class-visitor.php'
-	),
-	'Automattic\\Jetpack\\Terms_Of_Service' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-terms-of-service.php'
-	),
-	'Automattic\\Jetpack\\Tracking' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/src/class-tracking.php'
 	),
 	'Automattic\\Jetpack_CRM\\Onboarding_Wizard\\Bootstrap' => array(
 		'version' => 'dev-trunk',
@@ -737,34 +521,6 @@ return array(
 	'Hook_Manager' => array(
 		'version' => '2.11.21.0',
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/class-hook-manager.php'
-	),
-	'Jetpack_IXR_Client' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/legacy/class-jetpack-ixr-client.php'
-	),
-	'Jetpack_IXR_ClientMulticall' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/legacy/class-jetpack-ixr-clientmulticall.php'
-	),
-	'Jetpack_Options' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/legacy/class-jetpack-options.php'
-	),
-	'Jetpack_Signature' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/legacy/class-jetpack-signature.php'
-	),
-	'Jetpack_Tracks_Client' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/legacy/class-jetpack-tracks-client.php'
-	),
-	'Jetpack_Tracks_Event' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/legacy/class-jetpack-tracks-event.php'
-	),
-	'Jetpack_XMLRPC_Server' => array(
-		'version' => '1.52.0.0',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-connection/legacy/class-jetpack-xmlrpc-server.php'
 	),
 	'Latest_Autoloader_Guard' => array(
 		'version' => '2.11.21.0',
