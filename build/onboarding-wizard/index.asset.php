bbc65f0f954c38441625d1b221f30439d48db224
monsieur-z <monsieur-z@users.noreply.github.com>
2023-07-03 13:38:18 +0000

    My Jetpack: Organize cards in list (#31606)
    * [not verified] My Jetpack: organize cards in list
    
    * [not verified] changelog
    
    * [not verified] Fix formatting issue in MD files
    
    * [not verified] Update changelog message
    
    * Fix vertical margin between cards
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5444970235

--- build/onboarding-wizard/index.asset.php
+++ build/onboarding-wizard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '25ff0dbe7b6caef27007');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '7a54f3bf57a63d725e10');
