d46d1d2aae4482471d76f52d91280e45a7300c67
kallehauge <kallehauge@users.noreply.github.com>
2023-07-18 19:38:58 +0000

    CRM: Create Automations UI root (#31853)
    * [not verified] Initial admin root
    
    * [not verified] changelog
    
    * [not verified] Add header
    
    * [not verified] Add feature flag
    
    * [not verified] Add tests
    
    * [not verified] Update TODO
    
    * [not verified] Add newline
    
    * [not verified] Fix menu by using Core.Menus
    
    * [not verified] Revert extraneous changes
    
    * Rename automations component to automations-admin
    
    * Create admin-page-init.php
    
    * Use ThemeProvider
    
    * Update changelog
    
    * Add feature flags to menus
    
    * Adhere to monorepo expectations
    
    * Automation: move admin init file into admin dir
    
    * Prevent direct file access to automations main page
    
    * Correct load admin page hook
    
    * Rename core automations slug
    
    We currently have a paid extension using the same slug, so if that has been enabled, we're having issues with overrides.
    E.g.: the CRM system will not recognize the new page as an admin page.
    
    * Refactor admin page to use a CRM generic admin page wrapper
    
    * Remove unused style file
    
    ---------
    
    Co-authored-by: André Kallehauge <3846700+kallehauge@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5591490385

---
+++ build/automations-admin/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'e7b9ca7f2901d996f36c');
--- build/onboarding-wizard/index.asset.php
+++ build/onboarding-wizard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '90362ac1dce914bd17fb');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'bb2758ee5dfef23f7fea');
