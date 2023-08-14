0e4727858b7c05ce11491a3d345c911df2bb59f4
Initsogar <Initsogar@users.noreply.github.com>
2023-08-14 12:30:01 +0000

    Dashboard: Add backup undo feature (#32442)
    * Add endpoint to fetch backup undo event
    
    * changelog
    
    * Add backup undo event API
    
    * Add actions/reducers to handle undo event fetch
    
    * Refactor undo event reducer
    
    * Add component to query backup undo event
    
    * Add backup undo feature in backup card
    
    * Enable undo feature for WoA
    
    * Update projects versions
    
    * Move undo-event API to backup REST controller
    
    * Fetch undo event only when rewind status is active
    
    * Adjust track events for backup undo view and click
    
    * Update projects/plugins/jetpack/_inc/client/components/data/query-backup-undo-event/index.jsx
    
    Co-authored-by: Rustam Shaikh <rustam.shaikh@automattic.com>
    
    ---------
    
    Co-authored-by: Rustam Shaikh <rustam.shaikh@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5855480389

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '383eb9f6bd9f4a0db7b9');
+<?php return array('dependencies' => array('react', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'aafa2148d7d902e2c608');
--- build/onboarding-wizard/index.asset.php
+++ build/onboarding-wizard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'f1f215b3adef3de781d4');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '11341ebc9b03107f0a7c');
