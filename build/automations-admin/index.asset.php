9bee3ab20eb99397df7cdcc63899f7ac90922db7
robertf4 <robertf4@users.noreply.github.com>
2023-08-25 21:17:48 +0000

    CRM Automations: Add workflow table (#32443)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5980364412

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'dbdb6b9e226000208066');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '66fa04a61bffca31ccc4');
