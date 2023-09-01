cfd37709cfe320d2fbda937efcd04e2652ba9109
robertf4 <robertf4@users.noreply.github.com>
2023-09-01 15:54:35 +0000

    CRM Automations: Add Bulk Workflow Actions Component (#32448)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6051334685

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '66fa04a61bffca31ccc4');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '7097d289d70cd6bb9a37');
