1cc6fcbc4c1968fef00db1e549a28e3538bb8c6a
kallehauge <kallehauge@users.noreply.github.com>
2023-09-29 21:22:13 +0000

    [CRM] Automation UI: Remove test pages (#33418)
    * [CRM] Automation UI: Remove test pages
    
    * Changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6357079618

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '3d96ad01ef3c1eb58b49');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '6f6b395efe05ce643765');
