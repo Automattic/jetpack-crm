effd5caa36072e7b4431b508208369a7d654bb64
kallehauge <kallehauge@users.noreply.github.com>
2023-09-29 21:56:31 +0000

    CRM Automations UI: Add TriggerInfo and use next_step_true (#33416)
    * Change from next_step to next_step_true
    
    * changelog
    
    * Add TriggerInfo
    
    * Code standards
    
    * Hide overflow-x when scrolling is enabled
    
    ---------
    
    Co-authored-by: André Kallehauge <3846700+kallehauge@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6357423148

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '6f6b395efe05ce643765');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'bf4b8d380db548dbccf3');
