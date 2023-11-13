c6aaa87f8f9ff754bdbc9a2d5185635f8ac95e59
anomiex <anomiex@users.noreply.github.com>
2023-11-13 22:20:05 +0000

    Update dependency axios to v1 [SECURITY] (#34063)
    * Update dependency axios to v1 [SECURITY]
    * Fix typescript type import
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6856295457

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '94452efa4bcaa651d1c2');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '13accd4c78f44757696b');
