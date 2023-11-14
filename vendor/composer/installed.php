86587e3349a6345a4649ca0ec94ace9e07dbe8b6
zinigor <zinigor@users.noreply.github.com>
2023-11-14 16:22:21 +0000

    Update to node 20 (#33818)
    * Update to node 20
    
    Calypso has updated to node 20, which means it's time for us to do so as
    well.
    
    Also, it has been two years since we switched from yarn to pnpm. Let's
    clean up the dummy `.engines.yarn` and remove the doc it references.
    
    Also, since pnpm is the only thing (as far as I know) we use that checks
    package.json `.engines` and it's happy to check the monorepo root for
    everything, let's remove the engines from all the projects'
    package.jsons so we don't have to have such a giant PR in the future.
    That means we can also get rid of the linting that syncs all the
    projects' package.jsons and the special logic in the mirroring that
    strips it out of the mirrors.
    
    * assets: Update expected error message in test
    
    * d'oh
    
    * version bumps
    
    ---------
    
    Co-authored-by: Igor Zinovyev <zinigor@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6866313594

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'automattic/jetpack-assets' => array(
-            'pretty_version' => '1.18.15-alpha.1699908185',
-            'version' => '1.18.15.0-alpha1699908185',
-            'reference' => '008a5edfdc60c6614ae801e96192164c39f17d06',
+            'pretty_version' => '1.18.15-alpha.1699978088',
+            'version' => '1.18.15.0-alpha1699978088',
+            'reference' => '190261854590725d328410e5974be380f6c90ced',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-autoloader' => array(
             'pretty_version' => '2.12.0',
             'version' => '2.12.0.0',
-            'reference' => 'd6ee18610329c35c3cedd9b62cf92cd1417fe216',
+            'reference' => 'a55255daaef4180a4879c0f46c169ab0ac9c4a3a',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-composer-plugin' => array(
             'pretty_version' => '1.1.14',
             'version' => '1.1.14.0',
-            'reference' => '3832522ed83e108844a940cff909e2d6a08f3902',
+            'reference' => '148d1e3c742bb9775f8b6a2340601d34388419e4',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-composer-plugin',
             'aliases' => array(),
@@ -40,7 +40,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '1.6.23',
             'version' => '1.6.23.0',
-            'reference' => '0493fdc449e842d9d5d655f36be8c567de277f19',
+            'reference' => '9368da481f1ae55c4a11e2de1dd25b536dcfcf2d',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-constants',
             'aliases' => array(),
