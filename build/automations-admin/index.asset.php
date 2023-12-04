ee16dcbda3509df9e30c6b4c0299acdf8cf8ea7f
tbradsha <tbradsha@users.noreply.github.com>
2023-12-04 18:24:39 +0000

    CRM: Resolves 3389 - remove axios dependencies (#34315)
    * Add key props and wrap table data in proper tags
    
    * Replace axios with native fetch
    
    * Add changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7090736127

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '089aa7a0c416fd294437');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '31bd7d054d404a4e9fee');
