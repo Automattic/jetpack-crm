951b6ee2af14ba9ae5f2847962293502d8239dbf
thingalon <thingalon@users.noreply.github.com>
2023-12-19 05:31:15 +0000

    Boost: Refactor module, index and more into React (#34662)
    * Create skeleton for Module component in React
    
    * Refactor UpgradeCTA to React
    
    * Added a small variant for toggle control
    
    * Use small variant of toggle control
    
    * Fix onEnable, onMountEnable, and onDisable
    
    * Do not pass yearlyPricing
    
    * Implement onEnable and onDisable
    
    * changelog
    
    * Create index component
    
    * Make module errors consistent with old design
    
    * Fix ISA alignment
    
    * Improve alignments and spacing
    
    * Add resize unavailable notice
    
    * Add fake recommendations meta
    
    * Add missing pieces
    
    * Cleanup
    
    * Remove Index.svelte
    
    * Move pricing to ds based config
    
    * Remove Module.svelte
    
    * Use React DS based moduleState management
    
    * Restore tsconfig change
    
    * Change interface for useModuleState callbacks
    
    * Call useModuleState hook directly on the component
    
    * Fix infinite loop on purchase success
    
    * Fixup project version
    
    * Fix selector in test
    
    * Make speed score dependent on useModulesState for auto-refresh to work
    
    * Fix isModuleEnabled check
    
    * Update modules-state endpoint in tests
    
    ---------
    
    Co-authored-by: Mark George <thingalon@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7257710528

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '0814ed6e08e25d17d4f7');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8488d6aa692b502d71a0');
