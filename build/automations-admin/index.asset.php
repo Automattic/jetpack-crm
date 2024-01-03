6c1b2ae9ab4cd8e54fa5b1cf779b157ab83eb267
darssen <darssen@users.noreply.github.com>
2024-01-03 18:44:29 +0000

    Fix/jetpack footer inconsistent behavior (#34787)
    * About link shows now as not external in admin-links scenario
    
    * Added siteadminUrl for AdminPage component to pass it to JetpackFooter
    
    * Added getSiteAdminUrl tool in components.
    
    * Refactored JetpackFooter and its usages to use getSiteAdminUrl from tools instead from props.
    
    * Add changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7401056719

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'd2eb6358c626bb06dc20');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e08d708c4a8735a3b46d');
--- build/onboarding-wizard/index.asset.php
+++ build/onboarding-wizard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '210302ba1c501d7482b4');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'fe774d64ae886aa64aee');
