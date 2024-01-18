d8b3f0bd6292f2fc9cecd693ddc4926f0eb86f1e
cleacos <cleacos@users.noreply.github.com>
2024-01-18 15:04:58 +0000

    Jetpack Manage: Add a Jetpack Manage banner to My Jetpack (#35078)
    * Extract "could use" logic into its method could_use_jp_manage
    
    * Add is_agency_account to check if the user is a partner/agency
    
    * Pass the Jetpack Manage info to My Jetpack app
    
    * Create the UpsellBanner component to be reusable
    
    * Add the external link icon to the Gridicon component
    
    * Create the JetpackManageBanner component and add it to My Jetpack
    
    * changelog
    
    * Remove React from the import
    
    * Fix translation build issue.
    
    * Remove GridIcon component. It's not necessary using the isExternalLink prop.
    
    * Leave empty alt attribute
    
    * Remove import __ from @wordpress/i18n. Not used.
    
    * Update JetpackManageBanner with a better variable names.
    
    * Renaming the props prefix: cta1 => secondaryCta and cta2 => primaryCta
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7571523365

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'ceaf07519642fefa70be');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a45c01df578bbebedb7b');
