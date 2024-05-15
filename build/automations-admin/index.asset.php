6c36aa2bd015eac24da166b1afbae8cae5979859
anomiex <anomiex@users.noreply.github.com>
2024-05-15 16:35:42 +0000

    Update react monorepo to v18.3.1 (#37380)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9099249530
    
    Upstream-Ref: Automattic/jetpack@7f3e5f718176948d5cf8e7352561f0bc49f784ee

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9f9af708693d41675b71');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '42f33eaba25b588a7ab8');
--- build/onboarding-wizard/index.asset.php
+++ build/onboarding-wizard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'edd2332ace5e6fd9060b');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '0d4f704dee65b6a32518');
