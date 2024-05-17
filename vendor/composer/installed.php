49d48f5e9d79ad03914fe5801bb47ff82772155e
anomiex <anomiex@users.noreply.github.com>
2024-05-17 14:19:12 +0000

    changelogger: Normalize UTC timezone (#37436)
    It seems that some versions of git may be returning `Z` rather than
    `+00:00` when reporting timezones from `%cI`, which broke a test.
    Normalize it to `Z` always so the test can have a consistent value to
    expect.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9129465139
    
    Upstream-Ref: Automattic/jetpack@e80078a9d754cd5510bf194b4ed05d59a2c9d99d

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-assets' => array(
             'pretty_version' => '2.1.11-alpha.1715875343',
             'version' => '2.1.11.0-alpha1715875343',
-            'reference' => '8bb7ab859cc3cae427606990ff728018b9351d27',
+            'reference' => '527583bcdb95e346f065a5ecbb0b5e9131c4a4a4',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-autoloader' => array(
             'pretty_version' => '3.0.7',
             'version' => '3.0.7.0',
-            'reference' => '21276d036a0bcd574cf3d8096dd7f2df19eda774',
+            'reference' => '0b1f8ba4d969618e60c871bc19b3ffc68e0ad586',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-composer-plugin' => array(
             'pretty_version' => '2.0.1',
             'version' => '2.0.1.0',
-            'reference' => 'afdc4285903ce53b2700bc0bb6823bd61066d17d',
+            'reference' => 'af34e4d39665bb333211d97b9443e7b2c0fb3b39',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-composer-plugin',
             'aliases' => array(),
@@ -40,7 +40,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '2.0.2',
             'version' => '2.0.2.0',
-            'reference' => '2a3f3aab1a8fadcac1f4027999a836e2255ddc75',
+            'reference' => 'b4c692f6093c8b55637944bf2a7384a2465cce23',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-constants',
             'aliases' => array(),
