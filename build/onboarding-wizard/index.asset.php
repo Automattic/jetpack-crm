c0012ba76ea1532f9b2cd309fd1c1e5979608dc8
elliottprogrammer <elliottprogrammer@users.noreply.github.com>
2024-05-30 14:46:05 +0000

    Update connection ToS messaging (#37536)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9304377855
    
    Upstream-Ref: Automattic/jetpack@e9150a3b15305d9d578e5e09b28e3f63588267ef

--- build/onboarding-wizard/index.asset.php
+++ build/onboarding-wizard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '0d4f704dee65b6a32518');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a0fad7b282e961ebb335');
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-assets' => array(
             'pretty_version' => '2.1.11',
             'version' => '2.1.11.0',
-            'reference' => 'eb2fb866cd2b06805b07c2c87c2f3210f6f8181d',
+            'reference' => 'e58c6835332c35c62590f7a56f359796f2c6a806',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-autoloader' => array(
             'pretty_version' => '3.0.8',
             'version' => '3.0.8.0',
-            'reference' => '97ec3159be1ac60efb5ac5b0f6866ecb9193d9a6',
+            'reference' => '617fb0cc862974049d4af996717fcc92bf887567',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-composer-plugin' => array(
             'pretty_version' => '2.0.2-alpha.1717008994',
             'version' => '2.0.2.0-alpha1717008994',
-            'reference' => 'fce416473ccb084078efa4653a3bf1b1dfbf6a28',
+            'reference' => '9dc0ada62b38469e9ebec3bdc66edbdef9721881',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-composer-plugin',
             'aliases' => array(),
@@ -40,7 +40,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '2.0.3',
             'version' => '2.0.3.0',
-            'reference' => '5e7da63672d121c1b2defa3ff20b5965a7ddfc60',
+            'reference' => '3ea58f9d7b360b850308202a9a8765f13d65c8ea',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-constants',
             'aliases' => array(),
