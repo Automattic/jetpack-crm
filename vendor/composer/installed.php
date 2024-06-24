d7a3b01329dc661ab25402dd319f6626146992b9
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2024-06-24 06:58:31 +0000

    Social: Add Threads connection UI (#37977)
    * Update threads icon color
    
    * Add Threads to supported services for social connections
    
    * Add badges support for services list in the modal
    
    * Add changelogs
    
    * Fix up versions
    
    * Fix up versions
    
    * Ensure threads is loaded in the initial post meta
    
    * Add changelog
    
    * Update screenshot and description
    
    * Fix icon backdrop radius on Jetpack settings page
    
    * Add specificity to facebook icon style
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9640822445
    
    Upstream-Ref: Automattic/jetpack@bc30c21a6d0a205f52ad711d577accc7920eb3e0

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-assets' => array(
             'pretty_version' => '2.1.12',
             'version' => '2.1.12.0',
-            'reference' => '4623833cef11b9b97829ec2a46af0783fc653395',
+            'reference' => '98817a5902a8a680ebe37bbbf977af207092939d',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-autoloader' => array(
             'pretty_version' => '3.0.8',
             'version' => '3.0.8.0',
-            'reference' => '5a9572a915059b581f9ff767f1a1b34a3c102967',
+            'reference' => '831400278d513d69ab7b9d2d9b84f6491e563fac',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-composer-plugin' => array(
             'pretty_version' => '2.0.2',
             'version' => '2.0.2.0',
-            'reference' => 'ece99fbb655165c55b4fd7e1977abcf52f4cf758',
+            'reference' => '5dc2955e52b527ff3bef8fa47904e91e2d839c82',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-composer-plugin',
             'aliases' => array(),
@@ -40,7 +40,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '2.0.3',
             'version' => '2.0.3.0',
-            'reference' => 'a698c93627266b7b177f15d90edf25ddc9152067',
+            'reference' => '0ff4f86a5f0b8edf87ce3696ce474a5db5ed78fb',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-constants',
             'aliases' => array(),
