e21fb79b22b12356e152e2b46deb305d594f3d9d
anomiex <anomiex@users.noreply.github.com>
2024-08-21 21:04:22 +0000

    Update dependency yoast/phpunit-polyfills to ^1.1.1 (#39004)
    * Update dependency yoast/phpunit-polyfills to 1.1.0 || 1.1.1
    * Caret version
    * wpcomsh: Remove unnecessary phpunit dep.
      `yoast/phpunit-polyfills` already brings in an appropriate version of
      phpunit.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10497266284
    
    Upstream-Ref: Automattic/jetpack@480d5da856bf545238871951e102ca8a200b5495

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,7 +6,7 @@ return array(
   'packages' => array(
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '2.3.3',
+      'ver' => '2.3.4-alpha1724273335',
     ),
   ),
 );
