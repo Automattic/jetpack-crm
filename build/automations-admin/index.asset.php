03507e94c0768e75ce2983cef1253cac9e42ecde
d-alleyne <d-alleyne@users.noreply.github.com>
2024-09-06 20:28:34 +0000

    Disable terser conditional optimization (#39279)
    * Disable optimization of conditionals
    
    This prevents optimizations such as converting:
    
    condition ? __( 'true', 'domain' ) : __( 'false', 'domain' );
    
    to
    
    __(o?'true':'false','domain' );
    
    which is an invalid optimization for translatable strings
    
    * changelog
    
    * Update tests now that Terser's conditional optimization has been disabled
    
    * changelog
    
    * Remove documentation about unnecessary workaround
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10744633519
    
    Upstream-Ref: Automattic/jetpack@a4050bffedc64836cd9a41dfb7b700795bdd6a2b

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8ecb6a8845786fc0aada');
+<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'c48433adfdafe491cca0');
