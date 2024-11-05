e0804e8fc2874bb6a33565c3505132991f6520ba
anomiex <anomiex@users.noreply.github.com>
2024-11-05 22:08:36 +0000

    Update babel monorepo (#40060)
    * Update babel monorepo
    * search: Exclude corejs `exnext.iterator.` polyfills to avoid bloating the bundle.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11693139393
    
    Upstream-Ref: Automattic/jetpack@41ee4b3f61d85cead1eb2633e345358e62fb8cfd

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-assets' => array(
             'pretty_version' => '2.3.13',
             'version' => '2.3.13.0',
-            'reference' => 'e5de2918692c78f6b4abb5502ec2f3abb6a95bd8',
+            'reference' => '5ca303a54037c913fc03e9d9f3b37135359bf6ae',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-autoloader' => array(
             'pretty_version' => '3.1.3',
             'version' => '3.1.3.0',
-            'reference' => 'ac0eaaabe7115191c07cf958522e76304e0a4af2',
+            'reference' => '96d913aee2849998471a3841e16686ce74ce175c',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-composer-plugin' => array(
             'pretty_version' => '2.0.4',
             'version' => '2.0.4.0',
-            'reference' => '46c36d18d84ed2c19b9ec197caac130961cd1f51',
+            'reference' => 'd6b64a50c711e79895c294d77df9238914235686',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-composer-plugin',
             'aliases' => array(),
@@ -40,7 +40,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '2.0.5',
             'version' => '2.0.5.0',
-            'reference' => 'd0d9c9717be33d9541926cd20568ca2459c693d9',
+            'reference' => 'f973469066bc9ea4fb17a2dd890ec83498f7ed6e',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-constants',
             'aliases' => array(),
