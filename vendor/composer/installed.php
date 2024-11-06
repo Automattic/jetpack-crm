9b57ba4a0e1a9dcc136cf3d7b3245e6fff2112dd
nateweller <nateweller@users.noreply.github.com>
2024-11-06 16:48:20 +0000

    Components: Add Threats DataView (#39754)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11707882972
    
    Upstream-Ref: Automattic/jetpack@5ebf1b78f6de1bb1d79ded4c9258559d6c70a1e2

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-assets' => array(
             'pretty_version' => '2.3.13',
             'version' => '2.3.13.0',
-            'reference' => '5ca303a54037c913fc03e9d9f3b37135359bf6ae',
+            'reference' => 'e130214ef93ce393eee3f051a24891a67b35b5cf',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-autoloader' => array(
             'pretty_version' => '3.1.3',
             'version' => '3.1.3.0',
-            'reference' => '96d913aee2849998471a3841e16686ce74ce175c',
+            'reference' => '28f8d7eeabb8869e9a752705a0b557e03632aa05',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-composer-plugin' => array(
             'pretty_version' => '2.0.4',
             'version' => '2.0.4.0',
-            'reference' => 'd6b64a50c711e79895c294d77df9238914235686',
+            'reference' => '24266308916197bafc5f4d8ae2fd7521a7ff6204',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-composer-plugin',
             'aliases' => array(),
@@ -40,7 +40,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '2.0.5',
             'version' => '2.0.5.0',
-            'reference' => 'f973469066bc9ea4fb17a2dd890ec83498f7ed6e',
+            'reference' => 'e1cdd374ee9c43f4ffd647e05674f1cdb75ca0eb',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-constants',
             'aliases' => array(),
