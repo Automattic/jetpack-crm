36b99704b24e644cbdb0671e6d7574faf7787816
Initsogar <Initsogar@users.noreply.github.com>
2024-11-14 21:12:43 +0000

    Backport backup 3.0 Changes (#40194)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11845380582
    
    Upstream-Ref: Automattic/jetpack@13cb3bcaab6a6fe3c319031ea0a22c5539d8ca18

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,7 +7,7 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'3773ef3f09c37da5478d578e32b03a4b' => array(
-		'version' => '3.0.0.0-alpha1731605603',
+		'version' => '3.0.0.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 );
