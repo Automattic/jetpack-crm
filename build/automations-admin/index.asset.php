85a5b71ad504cacba0ecb883290e7150d54e6363
anomiex <anomiex@users.noreply.github.com>
2024-12-10 17:45:24 +0000

    Lock file maintenance (#40515)
    * Lock file maintenance
    * Address Phan issues due to php-stubs/wordpress-stubs upgrade
      * Some typecasts to account for better Core type delcarations.
      * Suppress `current_user_can_for_blog` → `current_user_can_for_site`
        until we drop 6.6 support.
    * Hack around Core SimplePie renaming
    
      Core upgraded their copy-pasted SimplePie library, which included
      renaming all the classes. While they did provide back-compat aliases, we
      run into a few problems:
    
      * A method parameter type of `SimplePie_Item` won't accept
        `SimplePie\Item`, while `SimplePie\Item` won't work on WP 6.6.
      * It's really hard to make Phan happy for both the two runs (with 6.7
        stubs and with 6.6 stubs).
    
      The best I was able to come up with was this:
      * Turn on `enable_class_alias_support`.
      * Alias `Jetpack\SimplePie\<name>` to the appropriate classes, and use
        those aliases everywhere.
        * Have phan load only one of the two alias files, depending on which
      	run it's doing.
      * Merge all the test mocks into one file and load them from
        bootstrap.php.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12261510871
    
    Upstream-Ref: Automattic/jetpack@ceebd5264525bb78288ad3a311f3c43b0fbcbc7a

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '176bed79604b1f6bc1e7');
+<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '53b9b0572494739c79c8');
