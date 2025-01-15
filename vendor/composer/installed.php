1d37e1e5e3a735367d0adcf190e6fe1c8610d717
ellatrix <ellatrix@users.noreply.github.com>
2025-01-15 14:09:36 +0000

    Upsell nudge: fix click Safari, disable block (#40902)
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12789810154
    
    Upstream-Ref: Automattic/jetpack@c1371cc3d8f520eadf849d21ea76f49ec40a6e84

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-assets' => array(
             'pretty_version' => '4.0.2',
             'version' => '4.0.2.0',
-            'reference' => '0de104a3fef3cba7bd5e864310309a8834dd95c2',
+            'reference' => 'f989b865b99da857a5ba860a73d896dd183bfe7b',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-autoloader' => array(
             'pretty_version' => '5.0.0',
             'version' => '5.0.0.0',
-            'reference' => 'b9103b258a8aebf96367229349a6eb40c1526ba9',
+            'reference' => 'edc4b31fcf6c35faf0c36fde2ec7d067af1846c2',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-composer-plugin' => array(
             'pretty_version' => '4.0.0',
             'version' => '4.0.0.0',
-            'reference' => 'fd24dbedcdb35198a9ef03c31036027fbcb80fc1',
+            'reference' => 'c3e9e4303428595ab44007488c3086f624dc6b07',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-composer-plugin',
             'aliases' => array(),
@@ -40,7 +40,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '3.0.1',
             'version' => '3.0.1.0',
-            'reference' => 'f2454fb7920f3ca574f3bb25727db9f7edccb14e',
+            'reference' => 'c78246365675b4b47e2b23852dfeb93381482f1a',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-constants',
             'aliases' => array(),
