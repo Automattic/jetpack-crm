953bff40520abe218597528a91c54f844c56aefd
tbradsha <tbradsha@users.noreply.github.com>
2025-10-30 14:03:02 +0000

    Update dependency svgo to v4 (#45678)
    * Remove title and viewbox during optimization
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/18942786078
    
    Upstream-Ref: Automattic/jetpack@253c06832014df646d76a148903800262b384482

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-assets' => array(
             'pretty_version' => '4.3.11',
             'version' => '4.3.11.0',
-            'reference' => 'a3b7abf4dfc6090f0c8f101864c8e04008f5b288',
+            'reference' => '0681cd120ab60fd7380a8f55c999cbae96bf6fb1',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-autoloader' => array(
             'pretty_version' => '5.0.11',
             'version' => '5.0.11.0',
-            'reference' => '5ee2320faf4874ea8bd8599a6f45dfaf43e4acf0',
+            'reference' => '448ec1574fd50be2e092b6020c307b8ce4724407',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-composer-plugin' => array(
             'pretty_version' => '4.0.5',
             'version' => '4.0.5.0',
-            'reference' => 'a3ba68c34513114a5ab6911372f0b2f021d5b971',
+            'reference' => 'c6407d3911ff540e2d332bde27a482ca3cffcc51',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-composer-plugin',
             'aliases' => array(),
@@ -40,7 +40,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '3.0.8',
             'version' => '3.0.8.0',
-            'reference' => 'abda5f21481dce2b0cab076230d3f4a3693a17eb',
+            'reference' => '1265374d05e5779ee5f1ac94ad788cb9cf627751',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-constants',
             'aliases' => array(),
@@ -49,7 +49,7 @@
         'automattic/jetpack-status' => array(
             'pretty_version' => '6.1.0',
             'version' => '6.1.0.0',
-            'reference' => '9ef3b860ea11fee92432d82fc3f4cd3776d43c4e',
+            'reference' => '997d2a973f2f22259e4180efeb33489b8c26d976',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-status',
             'aliases' => array(),
