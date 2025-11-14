96dbcc5921051441423f3f9aa6ba474820c85015
anomiex <anomiex@users.noreply.github.com>
2025-11-14 14:53:37 +0000

    Update tanstack-query monorepo (#45932)
    * Update tanstack-query monorepo
    * Remove obsolete pnpmfile hack
    * Remove obsolete eslint-disable
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/19367841380
    
    Upstream-Ref: Automattic/jetpack@a6939a7374712944152af9888726362c2d16585d

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'df813c64b35947a87a34');
+<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'acce44369a109a6a676b');
