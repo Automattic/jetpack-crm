7dbe9acd3387412b641fa74ec6879ec8d488389c
keoshi <keoshi@users.noreply.github.com>
2025-12-02 11:28:39 +0000

    Jetpack Logo: add support for customized title in shared component. (#46162)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/19856538885
    
    Upstream-Ref: Automattic/jetpack@6dfb5b1b5eefee00b0f810994e587f50a0fc1798

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a3eee1e45a2b1bd1508f');
+<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'cae3b6312ee9de62d9a8');
