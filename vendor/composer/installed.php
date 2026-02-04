7374fe30ce48ab9e413bbf05cd7ea86ba3984af9
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2026-02-04 04:29:35 +0000

    Social: Fix modal scroll empty space on smaller screens (#46925)
    * NavigatorModal: Prevent scrolling for navigator screen for large content.
    
    * Use the fullscreen modal limited by height and width
    
    * Override default bottom margin for full screen modals
    
    * Add changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/21658157383
    
    Upstream-Ref: Automattic/jetpack@65078e0dd2fa0adb73131bc7abcdbe9e17aaed4a

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-assets' => array(
             'pretty_version' => '4.3.22',
             'version' => '4.3.22.0',
-            'reference' => '1dbb093d2c833d7ee5160cdcd2db42625d0af05b',
+            'reference' => '792ff32e70ca34621c2422ded9ef3b24027f9407',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-assets',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-autoloader' => array(
             'pretty_version' => '5.0.15',
             'version' => '5.0.15.0',
-            'reference' => '7ebbcc5bf1de21a88e1f3022a32c4fe8378333da',
+            'reference' => '46c022d7ec4f1bb111405eacefdaec5d38a6cd48',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'automattic/jetpack-composer-plugin' => array(
             'pretty_version' => '4.0.5',
             'version' => '4.0.5.0',
-            'reference' => '7aad79711da14f7d1500de787124f5c208e25a33',
+            'reference' => 'e2a60cb088dc4f139fe12102e92e086ea29658f0',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-composer-plugin',
             'aliases' => array(),
@@ -40,7 +40,7 @@
         'automattic/jetpack-constants' => array(
             'pretty_version' => '3.0.8',
             'version' => '3.0.8.0',
-            'reference' => '762ad8171fed7ea303bb37f5bf3d2f9a12ef5a2c',
+            'reference' => 'efedfed14b565abff86e3c60a02c01bb43971f6e',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-constants',
             'aliases' => array(),
@@ -49,7 +49,7 @@
         'automattic/jetpack-status' => array(
             'pretty_version' => '6.1.2',
             'version' => '6.1.2.0',
-            'reference' => '720674b7312d3d565d4251c845ee4194a8fc26d0',
+            'reference' => '2928d9933179f4a2442fb81b6f3fc8591146208c',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../../jetpack_vendor/automattic/jetpack-status',
             'aliases' => array(),
