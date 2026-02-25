cc5b97bbd62605f040565d6fffbc5983bd39b338
zaguiini <zaguiini@users.noreply.github.com>
2026-02-25 18:45:01 +0000

    Social admin: Expand toggle sections width to fill container (#47308)
    * Social admin: Expand toggle sections across container
    
    * Add changelog
    
    * Limit social content to 80chs
    
    * Limit social page width to 1040px
    
    * Add changelog entries.
    
    * Address text wrap feedback
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/22410244928
    
    Upstream-Ref: Automattic/jetpack@c444adccd02141de655db585814c1d4bc1a70f45

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '5570993ecb0eeeb19b8c');
+<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '45b03ed524437ac8dd9d');
