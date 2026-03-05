eb289e1744dc2aaf1e41b598a7579025d97d9d00
vianasw <vianasw@users.noreply.github.com>
2026-03-05 16:09:32 +0000

    Headers: fix aria-label (#47461)
    * Update admin-ui package
    
    * Add aria label
    
    * lockfile
    
    * Add changelog entries.
    
    ---------
    
    Co-authored-by: Changelog Bot <changelog-bot@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/22725944601
    
    Upstream-Ref: Automattic/jetpack@90726da10afb9427a0a3ffe95bcfb9979c4f2983

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '0cb1b1561bd9d5824979');
+<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '549fc524fa940ad363b2');
