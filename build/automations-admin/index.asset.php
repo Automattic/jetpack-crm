99a6d51542388027e6ad38057b11e6e9211d8be5
anomiex <anomiex@users.noreply.github.com>
2026-04-03 20:04:24 +0000

    Update dependency browserslist to v4.28.2 (#47941)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/23959804872
    
    Upstream-Ref: Automattic/jetpack@c0f730e2cc18905a95ef9f927c182e5d6919cab7

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '0c7382f32b71f53eec87');
+<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e96535447b627f6acb01');
