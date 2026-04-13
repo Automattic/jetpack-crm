f00eb9b50bf0b748cb56dda9896864659cd7bbc3
anomiex <anomiex@users.noreply.github.com>
2026-04-13 14:58:50 +0000

    Update dependency stylelint to v17.7.0 (#48068)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/24349642108
    
    Upstream-Ref: Automattic/jetpack@5c7c2cf6b641fe0944c712b55d45aa3f0e2c4b1b

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'be91a696afc6a0760e0e');
+<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '5730e9d7c8c8e579a5c2');
