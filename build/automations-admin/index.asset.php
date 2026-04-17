262df03ff181f835843c50149164161f5bd2b58c
simison <simison@users.noreply.github.com>
2026-04-17 20:21:05 +0000

    Componentry: remove Jetpack color overrides on core components (#47317)
    * Componentry: remove Jetpack color overrides on core components
    * Go straight to core buttons for Search dashboard
    * Don't import built wp-component styles on several dashboards
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/24584355850
    
    Upstream-Ref: Automattic/jetpack@b56bf55991082ed45de59322038fc97f64471d4c

--- build/automations-admin/index.asset.php
+++ build/automations-admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '5ad3458e845e6855ef02');
+<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9b87c520bd8c0da6a8b0');
