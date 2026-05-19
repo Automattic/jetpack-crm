4312061e22a6de98a3909e823be20f26b989b002
anomiex <anomiex@users.noreply.github.com>
2026-05-19 18:55:49 +0000

    Backport jetpack 15.9-a.3 changes (#48970)
    ---------
    
    Co-authored-by: robertsreberski <robert.sreberski@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/26117594569
    
    Upstream-Ref: Automattic/jetpack@fa6e70b7598a7f27c8df41668e14ea93b8127e20

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,7 +6,7 @@ return array(
   'packages' => array(
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.3.37-alpha1778782620',
+      'ver' => '4.3.37',
     ),
   ),
 );
