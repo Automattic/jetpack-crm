7cf526f91d05f6d7f81e5cfdfb1441234d135024
Donncha O Caoimh <5656673+donnchawp@users.noreply.github.com>
2026-08-17 10:42:59 +0100

    Render the icon in the drag-drop blocker overlay too
    The header was fixed to render the metabox icon, but the blocker overlay
    one line down still ran the title through esc_html(). Enter rearrange mode
    on a contact or company and it shows a literal <i class="heartbeat icon"></i>
    before "Activity", which is the bug this branch set out to fix.
    
    Same narrow wp_kses() allowlist as the header uses.

--- includes/ZeroBSCRM.MetaBox.php
+++ includes/ZeroBSCRM.MetaBox.php
@@ -878,7 +878,7 @@ function zeroBSCRM_do_meta_box_html( $box, $page, $hidden, $object, $minimised, 
 			echo '<div id="' . esc_attr( $box['id'] ) . '-box" class="zbs-metabox-body ' . esc_attr( $htmlClasses ) . ' ' . esc_attr( $extraClasses ) . '">' . "\n"; // $hidden_class.
 				call_user_func( $box['callback'], $object, $box );
 			echo '</div>'; // /.zbs-metabox-body
-			echo '<div id="' . esc_attr( $box['id'] ) . '-block" class="zbs-metabox-block"><div>' . esc_html( $box['title'] ) . '</div></div>'; // this is BLOCKER for drag-drop support -//<i class="arrows alternate icon"></i>
+			echo '<div id="' . esc_attr( $box['id'] ) . '-block" class="zbs-metabox-block"><div>' . wp_kses( $box['title'], array( 'i' => array( 'class' => true ) ) ) . '</div></div>'; // this is BLOCKER for drag-drop support -//<i class="arrows alternate icon"></i>
 		echo "</div>\n";
 }
 
